2e32c81876e2711cda82071d207b56fd1f5594fe
meganshi31 <69047233+meganshi31@users.noreply.github.com>
2021-06-15 17:03:44 +0530

    Add files via upload

--- Profile.js
+++ Profile.js
@@ -1,57 +1,52 @@
 import React from 'react'
-import { View, Text, ImageBackground,StyleSheet } from 'react-native'
+import { View, Text, ImageBackground,StyleSheet, Image } from 'react-native'
 import { Card , Avatar , Appbar } from 'react-native-paper'
+import { color } from 'react-native-reanimated'
 
 export default function Profile() {
     return (
-<ImageBackground style = {{height:'100%',width:'100%'}} source = {require('../assets/Background1.png')} > 
-<Appbar.Header style = {{width:'100%',backgroundColor:'grey'}}>
-      <Appbar.BackAction />
-      <Appbar.Content title='Faculty' subtitle="" />
-      <Appbar.Action icon="magnify"  />
-      <Appbar.Action icon="dots-vertical"/>
-    </Appbar.Header>
-            <Card style = {styles.card}>   
-            <ImageBackground style = {{height:'100%',width:'100%',flexDirection:'column',alignItems:'center',justifyContent:'center'}} source = {require('../assets/Background.png')} >     
-            
-            <Avatar.Text style = {{}} size={70} label="XD"/>
-            <View>
-                <Text style = {{fontSize:30,color:'darkblue'}} > Ali ABBAS </Text>
-            </View>
-            </ImageBackground>
-            </Card>
+        <View>
+        <View style={styles.card}>
 
-            <Card style = {styles.card1}>   
-            <ImageBackground style = {{height:'100%',width:'100%'}} source = {require('../assets/Background.png')} >     
-           
-            <View style = {{flexDirection:'column',justifyContent:'space-evenly',alignItems:'center'}}>
-                <Text style = {{fontSize:30,color:'darkblue',flexDirection:'row',justifyContent:'space-evenly',alignItems:'center',marginTop:70}} > Email: MK @gmail.com</Text>
-                <Text style = {{fontSize:30,color:'darkblue',flexDirection:'row'}} > Mobile : 6376332312</Text>
-            </View>
-            </ImageBackground>
-            </Card>
-            
-    </ImageBackground>
+<Image style={{width:30,height:30,marginLeft:9,borderRadius:360,marginTop:50}} 
+ source= {require('../assets/backarrow.png')} />
+<Text style={{marginLeft:50,fontSize:20,fontWeight:'bold',marginTop:-28}}>Profile</Text>
+<Image style={{width:110,height:110,borderRadius:360,marginLeft:120,marginTop:30}}
+ source={require('../assets/Course.jpg')} />
+ <Text style={{fontSize:18,marginLeft:110,marginTop:10}}>Ali Abbas Banna</Text>
+ <Text style={{color:'#828282',marginLeft:110}}>aliabbas@gmail.com</Text>
+
+</View>
+
+<View style={styles.card1}>
+<Text style={{marginTop:30,marginLeft:20,color:'black'}}> Name:   Ali Abbas</Text>
+<Text style={{marginTop:30,marginLeft:20}}> Email:   aliabbas@gmail.com</Text>
+<Text style={{marginTop:30,marginLeft:20}}> MobileNo:   1234567890</Text>
+<Text style={{marginTop:30,marginLeft:20}}> Department:  Computer Science</Text>
+
+
+
+
+</View>
+</View>
+
     )
 }
 
 const styles = StyleSheet.create({
  
     card:{
-        borderRadius:15,
-        flexDirection:'row',
-        
-        flex:1,
-        
+        borderRadius:30,
+        // flexDirection:'row',
        
         height:300,
-        width:390,
-        marginLeft:10,
-        marginRight:10,
-        backgroundColor:'lightgrey',
-        
-        alignContent:'center',
-        alignItems:'center',
+        width:360,
+        // marginLeft:10,
+        // marginRight:10,
+        backgroundColor:'#E8F4F6',
+       
+        // alignContent:'center',
+        // alignItems:'center',
         elevation:3,
         shadowOffset:{width:100,height:100},
         shadowColor:'#000',
@@ -63,29 +58,24 @@ const styles = StyleSheet.create({
 
 
     card1:{
-        borderRadius:15,
-        flexDirection:'row',
-        flex:1,
-      
-        marginLeft:10,
-          
-        marginTop:10,
-       marginRight:5,
-        height:400,
-        width:390,
+        borderRadius:30,
+        // flexDirection:'row',
+       marginTop:40,     
+        height:380,
+        width:300,
+        marginLeft:26,
+        // marginRight:10,
+        backgroundColor:'#F2F8F9',
        
-        backgroundColor:'lightgrey',
-        
-        
-        
+        alignContent:'center',
+        // alignItems:'center',
         elevation:3,
         shadowOffset:{width:100,height:100},
         shadowColor:'#000',
         shadowOpacity:0.5,
         shadowRadius:2,
         fontSize:19,
+    }
             
-    },
-
-
+   
 })
